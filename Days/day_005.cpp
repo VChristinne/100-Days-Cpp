c212af736e9f4d696bf11557bca0242e7414bab4
Christinne <vanessa0christinne@gmail.com>
2024-03-15 19:28:01 +0100

    Add array2D

--- Days/day_005.cpp
+++ Days/day_005.cpp
@@ -1,6 +1,9 @@
 /*
- * Data Structures:
+ * Data Structures: a way of storing data and permits some operations on the data.
  * Array: sequence of elements of the same data type, include the contained type and the length.
+    * One-dimensional: store multiple values of similar data types;
+    * Two-dimensional: arrange elements in rows and columns;
+    * Three-dimensional: collection of two-dimensional arrays pilled on top of each other.
  * Linked List:
  * Stack:
  * Queue:
@@ -10,24 +13,28 @@
 #include <iostream>
 using namespace std;
 
-void array_type();
+void array_1D();
+void array_2D();
 
 int main() {
     ios_base::sync_with_stdio(false);
-    array_type();
+    printf("*** Array 1D ***");
+    array_1D();
+    printf("\n\n*** Array 2D ***\n");
+    array_2D();
 }
 
-void array_type() {
+void array_1D() {
     int my_array[100];
 
     int arr[] = {1, 2, 3, 4};
     printf("\nThird element: %i", arr[2]);
     arr[2] = 30;
-    printf("\nThird element: %i", arr[2]);
+    printf("\nThird element: %i", *(arr + 2));
 
     unsigned int maximum = 0;
     unsigned int minimum = UINT_MAX;
-    unsigned int values[] = {10, 50, 20, 40, 0};
+    unsigned int values[] = {0, 50, 20, 40, 0};
     for (unsigned int value : values) {
         if (value > maximum) maximum = value;
         if (value < minimum) minimum = value;
@@ -35,6 +42,27 @@ void array_type() {
     printf("\nThe maximum value: %i", maximum);
     printf("\nThe minimum value: %i", minimum);
 
-    size_t n_elements = sizeof(values) / sizeof(values[0]);
-    printf("\nSize: %lu", n_elements);
+    printf("\nSize of arr[0]: %lu", sizeof(arr[0]));
+    printf("\nSize of arr: %lu", sizeof(arr));
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("\nLength of an array: %i", n);
+}
+
+void array_2D() {
+    int arr[4][4];
+
+    // initialize 2D
+    for (int i=0; i<4; i++) {
+        for (int j=0;j<4; j++) {
+            arr[i][j] = i+j;
+        }
+    }
+
+    for (auto& i : arr) {
+        for (int j : i) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
 }
